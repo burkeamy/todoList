915e62588177bb8bae5c5169f2828ecf6260bdc2
unknown <burkeamy30@gmail.com>
2020-02-02 21:12:37 -0500

    up to version 8 of the todo list. Working in the DOM now.

--- main.js
+++ main.js
@@ -1,12 +1,13 @@
 let todoList  = {
     todos: [],
     displayTodos: function () {
+        //debugger;
         if(this.todos.length === 0) {
             console.log('You have nothing planned')
         } else {
             console.log('My todos: ');
             for (i = 0; i< this.todos.length; i++) {
-                if(this.todos.complete === true) {
+                if(this.todos[i].complete === true) {
                     console.log('(X)', this.todos[i].todoText)
                 } else {
                     console.log('( )', this.todos[i].todoText)
@@ -31,43 +32,57 @@ let todoList  = {
     },
     toggleCompleted: function(position) {
         let todo = this.todos[position];
-        todo.completed = !todo.completed;
+        todo.complete = !todo.complete;
         this.displayTodos()
     },
     toggleAll: function(){
         let totalTodos = this.todos.length;
         let completedTodos = 0;
         for(let i = 0; i< this.todos.length; i++){
-            if(this.todos[i].completed === true){
+            if(this.todos[i].complete === true){
                 completedTodos++;
             }        
         }
         if(completedTodos === totalTodos){
             for(let j = 0; j<this.todos.length; j++){
-                this.todos[j].completed = false;
+                this.todos[j].complete = false;
             }
         } else {
             for(let k = 0; k<this.todos.length; k++){
-                this.todos[k].completed = true;
+                this.todos[k].complete = true;
             }
         }
         this.displayTodos();
     }
 };
 
-/*todoList.displayTodos();
-todoList.addTodo('eat dinner');
-todoList.addTodo('play a game');
-//todoList.toggleCompleted(0,1);
-todoList.toggleAll();*/
-
-/*const displayTodosButton = document.getElementById('displayTodosButton');
-
-displayTodosButton.addEventListener('onclick', function () {
-    todoList.displayTodos();
-    //console.log('click')
-});*/
-
-document.getElementById('displayTodosButton').addEventListener("click", function(){
-    document.getElementById('demo').innerHTML = "Hello World";
-  });
+let handlers = {
+    displayTodos: function() {
+        todoList.displayTodos() 
+    },
+    toggleAll: function() {
+        todoList.toggleAll();
+    },
+    addTodo: function() {
+        let addTodoTextInput = document.getElementById('addTodoTextInput');
+        todoList.addTodo(addTodoTextInput.value);
+        addTodoTextInput.value = '';
+    },
+    changeTodo: function() {
+        let changeTodoPositionInput = document.getElementById('changeTodoPositionInput');
+        let changeTodoTextInput = document.getElementById('changeTodoTextInput');
+        todoList.changeTodos(changeTodoPositionInput.valueAsNumber, changeTodoTextInput.value);
+        changeTodoPositionInput.value = '';
+        changeTodoTextInput.value = '';
+    },
+    deleteTodo: function() {
+        let deleteTodoPositionInput = document.getElementById('deleteTodoPositionInput'); 
+        todoList.deleteTodo(deleteTodoPositionInput.valueAsNumber);
+        deleteTodoPositionInput.value = '';
+    },
+    toggleCompleted: function () {
+        let toggleCompletedPositionInput = document.getElementById('toggleCompletedPositionInput');
+        todoList.toggleCompleted(toggleCompletedPositionInput.valueAsNumber);
+        toggleCompletedPositionInput.value = '';
+    }
+}
